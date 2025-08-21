a281a0263cc2dfdcba113a3d6c502a7e6f696c0f
Farzin Naseri <129177949+FarzinizraF@users.noreply.github.com>
2025-08-21 18:39:17 +0330

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}", "../src/js/theme.js"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      width: {
+        22: "5.5rem",
+      },
+      inset: {
+        18: "4.5rem",
+      },
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+  },
+  plugins: [],
+};
